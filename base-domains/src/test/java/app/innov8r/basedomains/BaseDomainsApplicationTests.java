800ba8c7f9b4bcd341ac034549a6612f0782b54d
vikkastaneja <vikkastaneja@users.noreply.github.com>
2023-06-27 01:09:42 -0700

    Added order related DTOs in base domains

---
+++ base-domains/src/test/java/app/innov8r/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package app.innov8r.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
