f32943a3e6ba7efd51d3e13298f8b5dcd1d094b5
vikkastaneja <vikkastaneja@users.noreply.github.com>
2023-06-28 00:07:10 -0700

    Email service that is a consumer for "order_topic" kafka topic

---
+++ email-service/src/test/java/app/innov8r/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.innov8r.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
